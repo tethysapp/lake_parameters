0df416ccb922f8552c490d819da520511ab0a0e9
Danisa <ddolder@hotmail.com>
2020-04-09 10:07:48 -0600

    report2

--- tethysapp/lake/app.py
+++ tethysapp/lake/app.py
@@ -12,7 +12,7 @@ class Lake(TethysAppBase):
     package = 'lake'
     root_url = 'lake'
     color = '#0c8fab'
-    description = 'Utah Lake Water Parameters'
+    description = 'Utah Lake Water Parameters is an App that allows you to see the distribution of a chosen parameter values through the Utah Lake'
     tags = '"Hydrology","CEEN 514", "Utah Lake"'
     enable_feedback = False
     feedback_emails = []
@@ -38,6 +38,51 @@ class Lake(TethysAppBase):
                 name='instructions',
                 url='lake/datas/instructions',
                 controller='lake.controllers.instructions'
+            ),
+            UrlMap(
+                name='chl_a',
+                url='lake/datas/chl_a',
+                controller='lake.controllers.chl_a'
+            ),
+            UrlMap(
+                name='do',
+                url='lake/datas/do',
+                controller='lake.controllers.do'
+            ),
+            UrlMap(
+                name='nit',
+                url='lake/datas/nit',
+                controller='lake.controllers.nit'
+            ),
+            UrlMap(
+                name='ph',
+                url='lake/datas/ph',
+                controller='lake.controllers.ph'
+            ),
+            UrlMap(
+                name='phosp',
+                url='lake/datas/phosp',
+                controller='lake.controllers.phosp'
+            ),
+            UrlMap(
+                name='water_temp',
+                url='lake/datas/water_temp',
+                controller='lake.controllers.water_temp'
+            ),
+            UrlMap(
+                name='tds',
+                url='lake/datas/tds',
+                controller='lake.controllers.tds'
+            ),
+            UrlMap(
+                name='turb',
+                url='lake/datas/turb',
+                controller='lake.controllers.turb'
+            ),
+            UrlMap(
+                name='secchi',
+                url='lake/datas/secchi',
+                controller='lake.controllers.secchi'
             ),
         )
 
--- tethysapp/lake/controllers.py
+++ tethysapp/lake/controllers.py
@@ -97,27 +97,89 @@ def home(request):
 @login_required()
 def add_data(request):
     """
-    Controller for the Add Data page.
+    Controller for the Search Data page.
     """
-
-    add_button = Button(
-        display_text='Add',
-        name='add-button',
-        icon='glyphicon glyphicon-plus',
-        style='success'
+    lake_map = MapView(
+        height='100%',
+        width='100%',
+        layers=[],
+        basemap='OpenStreetMap',
     )
 
-    cancel_button = Button(
-        display_text='Cancel',
-        name='cancel-button',
-        href=reverse('lake:home')
+
+    add_data_button = Button(
+        display_text='Add Data',
+        name='add-data-button',
+        icon='glyphicon glyphicon-plus',
+        style='success',
+        href=reverse('lake:add_data')
+    )
+
+    save_button = Button(
+        display_text='',
+        name='save-button',
+        icon='glyphicon glyphicon-floppy-disk',
+        style='success',
+        attributes={
+            'data-toggle':'tooltip',
+            'data-placement':'top',
+            'title':'Save'
+            }
+    )
+
+    edit_button = Button(
+        display_text='',
+        name='edit-button',
+        icon='glyphicon glyphicon-edit',
+        style='warning',
+        attributes={
+            'data-toggle':'tooltip',
+            'data-placement':'top',
+            'title':'Edit'
+        }
+    )
+
+    remove_button = Button(
+        display_text='',
+        name='remove-button',
+        icon='glyphicon glyphicon-remove',
+        style='danger',
+        attributes={
+            'data-toggle':'tooltip',
+            'data-placement':'top',
+            'title':'Remove'
+        }
+    )
+
+    previous_button = Button(
+        display_text='Previous',
+        name='previous-button',
+        attributes={
+            'data-toggle':'tooltip',
+            'data-placement':'top',
+            'title':'Previous'
+        }
+    )
+
+    next_button = Button(
+        display_text='Next',
+        name='next-button',
+        attributes={
+            'data-toggle':'tooltip',
+            'data-placement':'top',
+            'title':'Next'
+        }
     )
 
     context = {
-        'add_button': add_button,
-        'cancel_button': cancel_button,
+        'save_button': save_button,
+        'edit_button': edit_button,
+        'remove_button': remove_button,
+        'previous_button': previous_button,
+        'next_button': next_button,
+        'lake_map': lake_map,
+        'add_data_button': add_data_button
     }
-
     return render(request, 'lake/add_data.html', context)
 
 @login_required()
@@ -130,3 +192,93 @@ def instructions(request):
     }
 
     return render(request, 'lake/instructions.html', context)
+
+def chl_a(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/chl_a.html', context)
+
+def do(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/do.html', context)
+
+def nit(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/nit.html', context)
+
+def ph(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/ph.html', context)
+
+def phosp(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/phosp.html', context)
+
+def water_temp(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/water_temp.html', context)
+
+def tds(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/tds.html', context)
+
+def turb(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/turb.html', context)
+
+def secchi(request):
+    """
+    Controller for the Instructions page.
+    """
+
+    context = {
+    }
+
+    return render(request, 'lake/secchi.html', context)
