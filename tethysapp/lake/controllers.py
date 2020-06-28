051184d7e77ee16a0360af906f01e9419e1d476c
Danisa Dolder <ddolder@hotmail.com>
2020-06-27 18:54:00 -0600

    iconos y timeserie con todos los dias pero sin corregir ceros

--- tethysapp/lake/controllers.py
+++ tethysapp/lake/controllers.py
@@ -4,7 +4,7 @@ from tethys_sdk.permissions import login_required
 from tethys_sdk.gizmos import MapView, Button
 from tethys_sdk.gizmos import TimeSeries
 from django.http import HttpResponse, JsonResponse
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from csv import writer as csv_writer
 import plotly.graph_objs as go
@@ -20,27 +20,7 @@ from pandas import DataFrame
 
 @login_required()
 def home(request):
-    """
-    Controller for the app home page.
-    """
-
-    app_workspace = app.get_app_workspace()
-    file_path = os.path.join(app_workspace.path,"awqms_lake.csv")
-    dataLake = pd.read_csv(file_path)
-    dataJson = dataLake.to_dict()
-
-    lake_map = MapView(
-        height='100%',
-        width='100%',
-        layers=[],
-        basemap='OpenStreetMap',
-    )
-
-    context = {
-        'lake_map': lake_map,
-        'jsonLake':dataJson
-    }
-
+    context = getStations()
     return render(request, 'lake/home.html', context)
 
 @login_required()
@@ -56,49 +36,94 @@ def add_data(request):
     )
 
     context = {
-        'lake_map': lake_map,
-    }
+        'lake_map': lake_map
+            }
     return render(request, 'lake/add_data.html', context)
 
 @login_required()
 def instructions(request):
-    """
-    Controller for the Instructions page.
-    """
-
     context = {
     }
 
     return render(request, 'lake/instructions.html', context)
 
-
-def getData(characteristic):
+def getFiles():
     app_workspace = app.get_app_workspace()
     file_path = os.path.join(app_workspace.path, "awqms_lake.csv")
-    # file_path = os.path.join(app_workspace.path, "awqms_lake.xlsx")
-
-    # file_path_miller = os.path.join(app_workspace.path, "Miller_data.xlsx")
-    # dataLake_awqms = pd.read_excel(file_path)
-    # dataLake_miller = pd.read_excel(file_path_miller)
     file_path_miller = os.path.join(app_workspace.path, "Miller_data.csv")
     dataLake_awqms = pd.read_csv(file_path)
     dataLake_miller = pd.read_csv(file_path_miller)
-    values_awqms = dataLake_awqms[['Monitoring Location ID', 'Monitoring Location Name', 'Characteristic Name', 'Monitoring Location Latitude', 'Monitoring Location Longitude', 'Activity Start Date', 'Result Value', 'Result Unit', 'Detection Condition', 'Detection Limit Value1', 'Detection Limit Unit1']]
-    values_miller = dataLake_miller[['Monitoring Location ID', 'Monitoring Location Name', 'Characteristic Name', 'Monitoring Location Latitude', 'Monitoring Location Longitude', 'Activity Start Date', 'Result Value', 'Result Unit', 'Detection Condition', 'Detection Limit Value1', 'Detection Limit Unit1']]
+    dataLake_miller['Organization ID'] = 'BYU, Dr Miller'
+    # print(dataLake_miller['Organization ID'])
+    values_awqms = dataLake_awqms[['Organization ID', 'Monitoring Location ID', 'Monitoring Location Name', 'Characteristic Name', 'Monitoring Location Latitude', 'Monitoring Location Longitude', 'Activity Start Date', 'Result Value', 'Result Unit', 'Detection Condition', 'Detection Limit Value1', 'Detection Limit Unit1']]
+    values_miller = dataLake_miller[['Organization ID', 'Monitoring Location ID', 'Monitoring Location Name', 'Characteristic Name', 'Monitoring Location Latitude', 'Monitoring Location Longitude', 'Activity Start Date', 'Result Value', 'Result Unit', 'Detection Condition', 'Detection Limit Value1', 'Detection Limit Unit1']]
     dataLake_all = [values_awqms, values_miller]
     dataLake = pd.concat(dataLake_all)
-    # print(dataLake)
+    context = dataLake
+    return context
 
+def getStations():
+    dataLake = getFiles()
+    locations = dataLake['Monitoring Location ID'].unique()
+    # locations.sort()
+    # print(locations)
+    context = {}
+
+    lake_map = MapView(
+        height='100%',
+        width='100%',
+        layers=[],
+        basemap='OpenStreetMap',
+    )
+    context['lake_map'] = lake_map
+    context['all_stations'] = {}
+
+    for location in locations:
+        df = dataLake[dataLake['Monitoring Location ID'] == location]
+        if len(df) > 0:
+            lat = df['Monitoring Location Latitude'].tolist()[0]
+            lon = df['Monitoring Location Longitude'].tolist()[0]
+            organization = df['Organization ID'].tolist()[0]
+            station = df['Monitoring Location Name'].tolist()[0]
+            dataname = str(location)
+            context['all_stations'][dataname] = {'coords': [lat, lon], 'org': organization, 'station': station}
+    return context
+
+def completeSeries(df):
+    df['Date'] = pd.to_datetime(df['Activity Start Date'], format = "%m-%d-%Y")
+    df = df.sort_values(by = ['Date'])
+    dateslist = df['Date'].tolist()
+    inidate = dateslist[0]
+    findate = dateslist[-1]
+    alldates = []
+    curdate = inidate
+    while curdate <= findate:
+        alldates.append(curdate)
+        curdate = curdate + timedelta(days = 1)
+    df2 = pd.DataFrame(alldates, columns = ['Date'])
+    dfjoin = df2.merge(df, on = 'Date', how = 'left')
+    value = dfjoin['Result Value']
+    dates = dfjoin['Date']
+    valuesNumpy = value.to_numpy()
+    valuesNoNan = np.nan_to_num(valuesNumpy)
+    valuesFin = valuesNoNan.tolist()
+    alldates = dates.to_numpy().tolist()
+    return valuesFin, alldates
+
+def getData(characteristic):
+    dataLake = getFiles()
     param = dataLake['Characteristic Name'] == characteristic
     row = dataLake[param]
+    # print(row)
     locations = row['Monitoring Location ID'].unique()
     unit = row['Result Unit'].unique()
-    locations.sort()
-    print(locations)
+    # locations.sort()
+    # print(locations)
     context = {}
     context['characteristic'] = characteristic
     context['unit'] = unit
     context['csvLake'] = dataLake
+
     lake_map = MapView(
         height='100%',
         width='100%',
@@ -107,47 +132,25 @@ def getData(characteristic):
     )
     context['lake_map'] = lake_map
     context['all_data'] = {}
+
     for location in locations:
         df = dataLake[dataLake['Monitoring Location ID'] == location]
         if len(df) > 0:
             lat = df['Monitoring Location Latitude'].tolist()[0]
             lon = df['Monitoring Location Longitude'].tolist()[0]
+            organization = df['Organization ID'].tolist()[0]
             dataname = str(location)
             charac_id = row['Monitoring Location ID'] == location
             charac = row[charac_id]
-            value = charac['Result Value']
-            date = charac['Activity Start Date']
-            valuesNumpy = value.to_numpy()
-            valuesNoNan = np.nan_to_num(valuesNumpy)
-            valuesFin = valuesNoNan.tolist()
-            # sacar el 1
             responseObject = {}
+            valuesFin, alldates = completeSeries(charac)
             responseObject['values'] = valuesFin
-            responseObject['dates'] = date.to_numpy().tolist()
-            # borrar
-            # date1 = responseObject1['dates'][0]
-            # date_len = len(responseObject1['dates'])
-            # date2 = responseObject1['dates'][date_len - 1]
-            # start = datetime.datetime(date1)
-            # end = datetime.datetime(date2)
-            # date_complete = pd.date_range(start, end)
-            # print(responseObject1['dates'])
-            # print(date_complete)
-            # responseObject = {}
-            # responseObject['dates'] = date_complete
-            # responseObject['values'] = responseObject1['values']
-            # print(responseObject)
-            # print(responseObject1['values'])
-            #
-
-            context['all_data'][dataname] = {'coords': [lat, lon], 'data': responseObject}
+            responseObject['dates'] = alldates
+            context['all_data'][dataname] = {'coords': [lat, lon], 'org': organization, 'data': responseObject}
     return context
-
-
 
 def chl_a(request):
     context = getData('Chlorophyll a, uncorrected for pheophytin')
-    #print(context)
     return render(request, 'lake/show_data.html', context)
 
 def do(request):
